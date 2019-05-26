a023e0f92df4c1dc0b13ba1ed250a5af848f68aa
Simon Tooke <simon@tooke.com>
2019-05-26 18:08:43 -0400

    more work on jmc, cleanr up buildjdk

--- build-jmc.sh
+++ build-jmc.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-set -x
+echo "**** doesn't work yet****"
+return 
 
-# define toolchain
-XCODE_APP=`dirname \`dirname \\\`xcode-select -p \\\`\``
-XCODE_DEVELOPER_PREFIX=$XCODE_APP/Contents/Developer
-CCTOOLCHAIN_PREFIX=$XCODE_APP/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
-OLDPATH=$PATH
-export PATH=$TOOL_PREFIX/usr/bin:$PATH
-export PATH=$CCTOOLCHAIN_PREFIX/usr/bin:$PATH
+set -x
 
 # define buuld environment
 BUILD_DIR=`pwd`
@@ -16,57 +11,43 @@ pushd `dirname $0`
 PATCH_DIR=`pwd`
 popd
 TOOL_DIR=$BUILD_DIR/tools
-DOWNLOAD_DIR=$BUILD_DIR/tools/downloads
 JDK_DIR=$BUILD_DIR/$JDKBASE
-
-JAVA_HOME=/Users/stooke/dev/ojdk/tools/jdk8u202-b08/Contents/Home
-export JAVA_HOME
 
 JMC_REPO=https://hg.openjdk.java.net/jmc/jmc/
 JMC_BUILD_DIR=`pwd`/jmc
 
-HG=$TOOL_DIR/mercurial-4.9/hg
-MVN=$TOOL_DIR/apache-maven-3.6.1/bin/mvn
-
 clone_jmc() {
     cd `dirname $JMC_BUILD_DIR`
-    $HG clone $JMC_REPO $JMC_BUILD_DIR
-}
-
-download_mvn() {
-    cd "$DOWNLOAD_DIR"
-    curl -O http://muug.ca/mirror/apache-dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
-    cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/apache-maven-3.6.1-bin.tar.gz"
+    hg clone $JMC_REPO $JMC_BUILD_DIR
 }
 
 start_jmc_background() {
     cd "$JMC_BUILD_DIR"
     cd releng/third-party
-    $MVN p2:site
-    $MVN jetty:run&
+    mvn p2:site
+    mvn jetty:run&
 }
 
 build_jmc() {
     cd "$JMC_BUILD_DIR"
     cd core
-    $MVN clean install
+    mvn clean install
     cd ..
-    $MVN package
+    mvn package
 }
 
 clean_jmc() {
     cd "$JMC_BUILD_DIR"
-    $MVN clean
+    mvn clean
     cd core
-    $MVN clean
-    rm -fr $HOME/.m2/repository
+    mvn clean
+    #rm -fr $HOME/.m2/repository
 }
 
-download_mvn
+. $PATCH_DIR/tools.sh $TOOL_DIR mvn mercurial
 clone_jmc
-#clean_jmc
-start_jmc_background
+clean_jmc
+#start_jmc_background
 sleep 5
-build_jmc
+#build_jmc
 
--- buildjdk.sh
+++ buildjdk.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-set -x
-
-# define toolchain
-XCODE_APP=`dirname \`dirname \\\`xcode-select -p \\\`\``
-XCODE_DEVELOPER_PREFIX=$XCODE_APP/Contents/Developer
-CCTOOLCHAIN_PREFIX=$XCODE_APP/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
-OLDPATH=$PATH
-export PATH=$TOOL_PREFIX/usr/bin:$PATH
-export PATH=$CCTOOLCHAIN_PREFIX/usr/bin:$PATH
-
 # define JDK and repo
 JDKBASE=jdk
 DEBUG_LEVEL=release
@@ -21,118 +11,45 @@ BUILD_DIR=`pwd`
 pushd `dirname $0`
 PATCH_DIR=`pwd`
 popd
-TOOL_DIR=$BUILD_DIR/tools
-DOWNLOAD_DIR=$BUILD_DIR/tools/downloads
 JDK_DIR=$BUILD_DIR/$JDKBASE
 
-build_autoconf() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz
-	cd "$TOOL_DIR"
-	tar -xzf "$DOWNLOAD_DIR/autoconf-2.69.tar.gz"
-	cd autoconf-2.69
-	./configure --prefix=`pwd`
-	make install
-}
-
-build_freetype() {
-	cd "$DOWNLOAD_DIR"
-	curl -O https://nongnu.freemirror.org/nongnu/freetype/freetype-2.9.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/freetype-2.9.tar.gz"
-	cd freetype-2.9
-	./configure
-	make
-}
-
-build_mercurial() {
-	cd "$DOWNLOAD_DIR"
-	curl -O https://www.mercurial-scm.org/release/mercurial-4.9.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/mercurial-4.9.tar.gz"
-	cd mercurial-4.9/
-	make local
-}
-
-build_bootstrap_jdk8() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz"
-}
-
-build_bootstrap_jdk11() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jdk_x64_mac_hotspot_11.0.2_9.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/OpenJDK11U-jdk_x64_mac_hotspot_11.0.2_9.tar.gz"
-}
-
-build_bootstrap_jdk12() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L https://github.com/AdoptOpenJDK/openjdk12-binaries/releases/download/jdk-12%2B33/OpenJDK12U-jdk_x64_mac_hotspot_12_33.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/OpenJDK12U-jdk_x64_mac_hotspot_12_33.tar.gz"
-}
-
-buildtools() {
-	mkdir -p "$DOWNLOAD_DIR"
-	mkdir -p "$TOOL_DIR"
-
-	for tool in autoconf mercurial bootstrap_jdk12 ; do 
-		echo "building $tool"
-		build_$tool
-	done
-}
-
-export PATH=$OLDPATH
-export JAVA_HOME=$TOOL_DIR/jdk-12+33/Contents/Home
-export PATH=$TOOL_DIR/autoconf-2.69/bin:$PATH
-export PATH=$TOOL_DIR/mercurial-4.9:$PATH
-export PATH=$JAVA_HOME/bin:$PATH
-
-downloadjdksrc() {
-	cd $BUILD_DIR
-	hg clone http://hg.openjdk.java.net/jdk/jdk $JDK_DIR
-	cd $JDK_DIR
-	hg revert .
-}
-
-downloadjdk11devsrc() {
-	cd $BUILD_DIR
-	hg clone http://hg.openjdk.java.net/jdk-updates/jdk11u-dev $JDK_DIR
-	cd $JDK_DIR
-	hg revert .
+downloadjdkdevsrc() {
+	if ! test -d "$JDK_DIR" ; then
+		hg clone http://hg.openjdk.java.net/jdk/$JDKBASE "$JDK_DIR"
+	else
+		pushd "$JDK_DIR"
+		hg pull -u
+		popd
+	fi
 }
 
 patchjdk() {
-	cd $JDK_DIR
-	hg revert .
-	hg import --no-commit $PATCH_DIR/mac-jdk8u.patch
-	for a in hotspot jdk ; do 
-		cd $JDK_DIR/$a
-		hg revert .
-		hg import --no-commit $PATCH_DIR/mac-jdk8u-$a.patch
-	done
+	if test -f "$PATCH_DIR/mac-jdk.patch" ; then
+		pushd "$JDK_DIR"
+		hg import --no-commit $PATCH_DIR/mac-jdk.patch
+		popd
+	fi
 }
 
 configurejdk() {
-	cd $JDK_DIR
+	pushd "$JDK_DIR"
 	chmod 755 ./configure
 	./configure --with-toolchain-type=clang \
             --includedir=$XCODE_DEVELOPER_PREFIX/Toolchains/XcodeDefault.xctoolchain/usr/include \
             --with-debug-level=$DEBUG_LEVEL \
             --with-boot-jdk=$JAVA_HOME 
+	popd
 }
 
 buildjdk() {
-	cd $JDK_DIR
-	make images CONF=macosx-x86_64-server-$DEBUG_LEVEL
+	pushd "$JDK_DIR"
+	make images CONF=macosx-x86_64-normal-server-$DEBUG_LEVEL
+	popd
 }
 
-#buildtools
-#downloadjdksrc
-#patchjdk
+. $PATCH_DIR/tools.sh "$BUILD_DIR/tools" autoconf mercurial bootstrap_jdk12
+downloadjdkdevsrc
+patchjdk
 configurejdk
 buildjdk
 
