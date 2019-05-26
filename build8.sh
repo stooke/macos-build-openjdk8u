02c28f1f528be543135cb142d981095c6241e15e
Simon Tooke <simon@tooke.com>
2019-05-26 17:49:21 -0400

    added tools.sh, tested jdk8/11/javafx builds

--- build8.sh
+++ build8.sh
@@ -2,106 +2,26 @@
 
 set -x
 
-# define toolchain
-XCODE_APP=`dirname \`dirname \\\`xcode-select -p \\\`\``
-XCODE_DEVELOPER_PREFIX=$XCODE_APP/Contents/Developer
-CCTOOLCHAIN_PREFIX=$XCODE_APP/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
-OLDPATH=$PATH
-export PATH=$TOOL_PREFIX/usr/bin:$PATH
-export PATH=$CCTOOLCHAIN_PREFIX/usr/bin:$PATH
-
 # define JDK and repo
-JDKBASE=jdk8u
+JDKBASE=jdk8u-dev
 DEBUG_LEVEL=release
-DEBUG_LEVEL=slowdebug
+#DEBUG_LEVEL=slowdebug
 ## release, fastdebug, slowdebug
 
-# define buuld environment
+# define build environment
 BUILD_DIR=`pwd`
 pushd `dirname $0`
 PATCH_DIR=`pwd`
 popd
-TOOL_DIR=$BUILD_DIR/tools
-DOWNLOAD_DIR=$BUILD_DIR/tools/downloads
 JDK_DIR=$BUILD_DIR/$JDKBASE
-
-build_autoconf() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz
-	cd "$TOOL_DIR"
-	tar -xzf "$DOWNLOAD_DIR/autoconf-2.69.tar.gz"
-	cd autoconf-2.69
-	./configure --prefix=`pwd`
-	make install
-}
-
-build_freetype() {
-	cd "$DOWNLOAD_DIR"
-	curl -O https://nongnu.freemirror.org/nongnu/freetype/freetype-2.9.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/freetype-2.9.tar.gz"
-	cd freetype-2.9
-	./configure
-	make
-}
-
-build_mercurial() {
-	cd "$DOWNLOAD_DIR"
-	curl -O https://www.mercurial-scm.org/release/mercurial-4.9.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/mercurial-4.9.tar.gz"
-	cd mercurial-4.9
-	make local
-}
-
-build_bootstrap_jdk8() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz"
-}
-
-build_bootstrap_jdk10() {
-	cd "$DOWNLOAD_DIR"
-	curl -O -L https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz
-	cd "$TOOL_DIR"
-	tar -xvf "$DOWNLOAD_DIR/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz"
-}
-
-build_webrev() {
-	cd "$TOOL_DIR"
-	mkdir -p "$TOOL_DIR/webrev"
-	cd "$TOOL_DIR/webrev"
-	curl -O -L https://hg.openjdk.java.net/code-tools/webrev/raw-file/tip/webrev.ksh
-	chmod 755 webrev.ksh
-}
-
-buildtools() {
-	mkdir -p "$DOWNLOAD_DIR"
-	mkdir -p "$TOOL_DIR"
-
-	for tool in freetype autoconf mercurial bootstrap_jdk8 webrev ; do 
-		echo "building $tool"
-		build_$tool
-	done
-}
-
-export PATH=$OLDPATH
-export JAVA_HOME=$TOOL_DIR/jdk8u202-b08/Contents/Home
-export PATH=$TOOL_DIR/autoconf-2.69/bin:$PATH
-export PATH=$TOOL_DIR/mercurial-4.9:$PATH
-# export PATH=$TOOL_DIR/apache-ant-1.10.5-bin/bin:$PATH
-export PATH=$TOOL_DIR/webrev:$PATH
-export PATH=$TOOL_DIR/jtreg:$PATH
-export PATH=$JAVA_HOME/bin:$PATH
 
 downloadjdksrc() {
 	cd $BUILD_DIR
-	hg clone http://hg.openjdk.java.net/jdk8u/jdk8u-dev $JDK_DIR
+	hg revert .
+	hg clone http://hg.openjdk.java.net/jdk8u/$JDKBASE $JDK_DIR
 	cd $JDK_DIR
 	chmod 755 get_source.sh configure
 	./get_source.sh
-	hg revert .
 }
 
 patchjdk() {
@@ -118,24 +38,25 @@ patchjdk() {
 }
 
 configurejdk() {
-	cd $JDK_DIR
+	pushd $JDK_DIR
 	chmod 755 ./configure
 	./configure --with-toolchain-type=clang \
             --with-xcode-path=$XCODE_APP \
             --includedir=$XCODE_DEVELOPER_PREFIX/Toolchains/XcodeDefault.xctoolchain/usr/include \
             --with-debug-level=$DEBUG_LEVEL \
-            --with-boot-jdk=$TOOL_DIR/jdk8u202-b08/Contents/Home \
-            --with-freetype-include=$TOOL_DIR/freetype-2.9/include \
-            --with-freetype-lib=$TOOL_DIR/freetype-2.9/objs/.libs
+            --with-boot-jdk=$TOOL_DIR/jdk8u/Contents/Home \
+            --with-freetype-include=$TOOL_DIR/freetype/include \
+            --with-freetype-lib=$TOOL_DIR/freetype/objs/.libs
+	popd
 }
 
 buildjdk() {
-	cd $JDK_DIR
+	pushd $JDK_DIR
 	make images COMPILER_WARNINGS_FATAL=false CONF=macosx-x86_64-normal-server-$DEBUG_LEVEL
+	popd
 }
 
-buildtools
-#build_jtreg $JAVA_HOME
+. $PATCH_DIR/tools.sh $BUILD_DIR/tools freetype autoconf mercurial bootstrap_jdk8 webrev
 downloadjdksrc
 patchjdk
 configurejdk
---
+++ tools.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+# usage: . ./tools.sh tooldir [tool]...
+
+# define toolchain
+XCODE_APP=`dirname \`dirname \\\`xcode-select -p \\\`\``
+XCODE_DEVELOPER_PREFIX=$XCODE_APP/Contents/Developer
+CCTOOLCHAIN_PREFIX=$XCODE_APP/Contents/Developer/Toolchains/XcodeDefault.xctoolchain
+OLDPATH=$PATH
+export PATH=$TOOL_PREFIX/usr/bin:$PATH
+export PATH=$CCTOOLCHAIN_PREFIX/usr/bin:$PATH
+
+# define build environment
+TOOL_DIR="$1"
+if test ".$TOOL_DIR" = "." ; then 
+	TOOL_DIR="`pwd`/tools"
+fi
+if test ".$DOWNLOAD_DIR" = "." ; then
+	DOWNLOAD_DIR="$TOOL_DIR/downloads"
+fi
+if test ".$TMP" = "." ; then
+	TMP=/tmp
+fi
+if test ".$TMP_DIR" = "." ; then
+	TMP_DIR="$TMP/$$.work"
+fi
+
+download_and_open() {
+	URL="$1"
+	FILE="$DOWNLOAD_DIR/`basename $URL`"
+	DEST="$2"
+	if ! test -f "$FILE" ; then 
+		pushd "$DOWNLOAD_DIR"
+		curl -O -L "$URL"
+		popd
+	fi
+	if test -d "$DEST" ; then
+		return
+	fi
+	rm -fr "$TMP_DIR/dno"	
+	mkdir -p "$TMP_DIR/dno"	
+	pushd "$TMP_DIR/dno"	
+	tar -xvf "$FILE"
+	mv * "$DEST"
+	popd
+	rm -fr "$TMP_DIR/dno"	
+}
+
+build_autoconf() {
+	if test -d "$TOOL_DIR/autoconf" ; then
+		return
+	fi
+	download_and_open http://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz "$TOOL_DIR/autoconf"
+	pushd "$TOOL_DIR/autoconf"
+	./configure --prefix=`pwd`
+	make install
+	popd
+}
+
+build_cmake() {
+	if test -d "$TOOL_DIR/cmake" ; then 
+		return
+	fi
+	download_and_open https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3-Darwin-x86_64.tar.gz "$TOOL_DIR/cmake"
+}
+
+build_mvn() {
+	if test -d "$TOOL_DIR/apache-maven"; then
+		return
+	fi
+	download_and_open http://muug.ca/mirror/apache-dist/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz "$TOOL_DIR/apache-maven"
+}
+
+build_freetype() {
+	if test -d "$TOOL_DIR/freetype" ; then
+		return
+	fi
+	download_and_open https://nongnu.freemirror.org/nongnu/freetype/freetype-2.9.tar.gz "$TOOL_DIR/freetype"
+	pushd "$TOOL_DIR/freetype"
+	./configure
+	make
+	popd
+}
+
+build_mercurial() {
+	if test -f "$TOOL_DIR/mercurial/hg" ; then
+		return
+	fi
+	download_and_open https://www.mercurial-scm.org/release/mercurial-4.9.tar.gz "$TOOL_DIR/mercurial"
+	pushd "$TOOL_DIR/mercurial"
+	make local
+	popd
+}
+
+build_bootstrap_jdk8() {
+	if test -d "$TOOL_DIR/jdk8u" ; then
+		return
+	fi
+	download_and_open https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz "$TOOL_DIR/jdk8u"
+}
+
+build_bootstrap_jdk10() {
+	if test -d "$TOOL_DIR/jdk10u" ; then
+		return
+	fi
+	download_and_open https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_mac_hotspot_8u202b08.tar.gz "$TOOL_DIR/jdk10u"
+}
+
+build_bootstrap_jdk11() {
+	if test -d "$TOOL_DIR/jdk11u" ; then
+                return
+        fi
+        download_and_open https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.2%2B9/OpenJDK11U-jdk_x64_mac_hotspot_11.0.2_9.tar.gz "$TOOL_DIR/jdk11u"
+}
+
+build_webrev() {
+	if test -f "$TOOL_DIR/webrev/webrev.ksh" ; then
+		return
+	fi
+	pushd "$TOOL_DIR"
+	mkdir -p "$TOOL_DIR/webrev"
+	cd "$TOOL_DIR/webrev"
+	curl -O -L https://hg.openjdk.java.net/code-tools/webrev/raw-file/tip/webrev.ksh
+	chmod 755 webrev.ksh
+	popd
+}
+
+buildtools() {
+	mkdir -p "$DOWNLOAD_DIR"
+	mkdir -p "$TOOL_DIR"
+
+	for tool in $* ; do 
+		echo "building $tool"
+		build_$tool
+		if test $tool == "bootstrap_jdk8" ; then
+			export JAVA_HOME=$TOOL_DIR/jdk8u/Contents/Home
+		fi
+		if test $tool = "bootstrap_jdk11" ; then
+                        export JAVA_HOME=$TOOL_DIR/jdk11u/Contents/Home
+                fi
+	done
+}
+
+export PATH=$OLDPATH
+export PATH=$TOOL_DIR/apache-maven:$PATH
+export PATH=$TOOL_DIR/autoconf/bin:$PATH
+export PATH=$TOOL_DIR/cmake:$PATH
+export PATH=$TOOL_DIR/mercurial:$PATH
+# export PATH=$TOOL_DIR/apache-ant/bin:$PATH
+export PATH=$TOOL_DIR/webrev:$PATH
+export PATH=$TOOL_DIR/jtreg:$PATH
+
+## TODO: these lines don't work
+if test "$*" == "*bootstrap_jdk8*" ; then 
+	export JAVA_HOME=$TOOL_DIR/jdk8u/Contents/Home
+fi
+if test "$*" == "*bootstrap_jdk10*" ; then 
+	export JAVA_HOME=$TOOL_DIR/jdk10u/Contents/Home
+fi
+if test "$*" == *bootstrap_jdk11* ; then 
+	export JAVA_HOME=$TOOL_DIR/jdk11u/Contents/Home
+fi
+export PATH=$JAVA_HOME/bin:$PATH
+
+mkdir -p "$TMP_DIR"
+shift
+buildtools $*
+rm -fr "$TMP_DIR"
+
