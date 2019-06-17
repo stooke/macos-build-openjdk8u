dfaccee61c62df62c618a966ca4d112b6b16e217
Simon Tooke <simon@tooke.com>
2019-06-17 11:16:18 -0400

    added quarkus build

---
+++ buildquarkusio.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# GRAALVM_HOME=/Users/stooke/dev/ojdk/graal/vm/mxbuild/darwin-amd64/GRAALVM_SVM/graalvm-svm-20.0.0-beta.02-dev/Contents/Home
+# define build environment
+BUILD_DIR=`pwd`
+pushd `dirname $0`
+PATCH_DIR=`pwd`
+popd
+TOOL_DIR=$BUILD_DIR/tools
+. $PATCH_DIR/tools.sh "$TOOL_DIR" mx maven bootstrap_jdk8
+
+git clone https://github.com/quarkusio/quarkus.git
+
+cd quarkus
+./mvnw clean install
+
