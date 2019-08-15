94b11166e125a7c87166aef43f9ad0ec01267294
Simon Tooke <stooke@redhat.com>
2019-08-15 10:35:54 -0400

    better mkwebrev.sh

--- mkwebrev.sh
+++ mkwebrev.sh
@@ -1,33 +1,62 @@
 #!/bin/bash
 #set -x
-NUM=00
-PATCH_DIR=`pwd`/xx
+set -e
+
+SCRIPT_DIR=`pwd`/xx
 WEBREV_BASE=`pwd`/webrevs
-. $PATCH_DIR/tools.sh `pwd`/tools webrev mercurial
+. $SCRIPT_DIR/tools.sh `pwd`/tools webrev mercurial
 mkdir -p "$WEBREV_BASE"
-cd jdk8u*
+cd jdk11u*
 repos="jdk hotspot corba nashorn langtools jaxp jaxws"
+repos=""
 
 mkwebrev() {
-  REPO_DIR=$1
-  WEBREV_DIR=$2
-  pushd "$REPO_DIR" >/dev/null
+	# $1 path to repo $2 webrev dir $3 CR#
+  RD=$1
+  WD=$2
+  CR=`echo $3 | sed s/[^0-9]*//g`
+  pushd "$RD" >/dev/null
   N=`hg status | wc -l`
   if [ $N != 0 ] ; then
-    webrev.ksh
-    mv webrev "$WEBREV_DIR"
+    webrev.ksh -b -w -o "$WD" -c $CR
+    mv $WD/webrev/* $WD/webrev/..
+    rmdir $WD/webrev
   else
     echo "  (no differences)"
   fi
   popd >/dev/null
 }
 
-mkwebrev . $WEBREV_BASE/webrev.$NUM
+mkrevs() {
+	# $1 CR $2 NUM
+	find "$REPO_DIR" -name \*.rej  -exec rm {} \; 2>/dev/null || true
+	find "$REPO_DIR" -name \*.orig -exec rm {} \; 2>/dev/null || true
+	WEBREV_DIR=$WEBREV_BASE/jdk-$1
+	mkwebrev . $WEBREV_DIR/webrev.$2 $1
+	for a in $repos ; do 
+	  echo processing `pwd`/$a
+	  mkwebrev $a $WEBREV_DIR/webrev.$a.$2 $1
+	done
+}
 
-for a in $repos ; do 
-  echo processing `pwd`/$a
-  mkwebrev $a $WEBREV_BASE/webrev.$a.$NUM
-done
+revert() {
+	pushd "$REPO_DIR" >/dev/null
+	hg revert .
+	for a in $repos ; do 
+	  cd $a
+	  hg revert .
+	  cd ..
+	done
+	find "$REPO_DIR" -name \*.rej  -exec rm {} \; 2>/dev/null || true 
+	find "$REPO_DIR" -name \*.orig -exec rm {} \; 2>/dev/null || true
+	popd >/dev/null
+}
+
+#revert
+
+mkrevs 8214777-jdk11u 00
+
+exit 0
 
 ## to update s01
 echo "don't forget to run"
